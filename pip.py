dc75f5dfc8086025d9135e9bbce09ab1b45e2baa
Christopher Pelletier <chpel173@ulaval.ca>
2024-03-05 12:00:46 -0500

    try

--- pip.py
+++ pip.py
@@ -1,24 +1,25 @@
 import numpy as np
+from random import *
 
 
 a = np.array([0, 1, 3, 2, 3, 1, 0, 5, 6, 5, 3, 5, 0, 3, 6, 2, 6, 3, 0, 4, 3, 5, 6, 4, 0])
 chemins = a.reshape(5, 5)
 
-for i in range(5):
-    route = 0
-    retour = []
-    for j in range(5):
-        if i == j:
-            route += 0
-        elif retour[i] == [j, i]:
-            pass
-        elif i != j:
-            route += chemins[i][j]
-            retour.append([i, j])
+for k in range(200):
+    for i in sample(range(5), 5):
+        route = 0
+        retour = []
+        for j in sample(range(5), 5):
+            if i == j:
+                route += 0
+            elif i != j:
+                route += chemins[i][j]
+                retour.append([i, j])
 
-            i = j
-    print(route)
-    print(retour)
-print(chemins)
+                i = j
+        print(route)
+        print(retour)
+    print("Fin de la boucle {}".format(k))
+
 
 
